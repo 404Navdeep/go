6fd5429d55cf8e6b7641c1e8e08e176f0fb4db84
Mr-Singh-69 <navdeep13dps@gmail.com>
2025-11-07 16:50:04 +0530

    hmm

--- api/[id].js
+++ api/[id].js
@@ -2,29 +2,28 @@ import fs from "fs";
 import path from "path";
 
 export default async function handler(req, res) {
+  const { id } = req.query;
+
   const filePath = path.join(process.cwd(), "urls.json");
-  const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  const urls = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
-  const short = req.query.id;
-  const dest = data[short];
-
-  if (!dest) {
-    return res.status(404).send("Short link not found");
+  const destination = urls[id];
+  if (!destination) {
+    return res.status(404).send("Short link missing");
   }
 
   const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
 
 const WEBHOOK_URL = "https://webhook.site/348541dd-aff4-4c93-9b82-fbf680af8acd"; 
-
-fetch(WEBHOOK_URL, {
+  fetch(WEBHOOK_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      id: short,
-      ip: ip,
+      id,
+      ip,
       timestamp: new Date().toISOString(),
     }),
   }).catch(() => {});
 
-  return res.redirect(dest);
+  return res.redirect(destination);
 }
